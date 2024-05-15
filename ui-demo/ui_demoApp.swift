37c5432e239e635ed28d634be811f5bb495b497d
shubhamprashar <shubham.prashar@finbox.in>
2024-05-15 13:32:12 +0530

    Initial Commit

---
+++ ui-demo/ui_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ui_demoApp.swift
+//  ui-demo
+//
+//  Created by Shubham Prashar on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ui_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
