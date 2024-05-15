a0137ef340436c1fc163cbe7ce6cbb5b5f78377a
shubhamprashar <shubham.prashar@finbox.in>
2024-05-15 19:48:10 +0530

    added code for the playcard

--- ui-demo/ContentView.swift
+++ ui-demo/ContentView.swift
@@ -9,13 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack{
+            Color(.systemMint).ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 20.0) {
+                Image("wallpaperflare.com_wallpaper (1)")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(15)
+            
+                
+                HStack {
+                    Text("Windows XP")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    
+                    VStack{
+                        
+                        HStack{
+                            Image(systemName: "star.fill").foregroundColor(Color.orange)
+                            Image(systemName: "star.fill").foregroundColor(Color.orange)
+                            Image(systemName: "star.fill").foregroundColor(Color.orange)
+                            Image(systemName: "star.fill").foregroundColor(Color.orange)
+                            Image(systemName: "star.leadinghalf.filled").foregroundColor(Color.orange)
+                        }
+                        
+                        Text("(reviews 5K)").fontWeight(.medium).foregroundColor(Color.orange)
+                            .font(.caption)
+                    }
+                }
+                
+                
+                Text("The OG of all operating systems")
+                    .font(.footnote)
+                    .fontWeight(.thin)
+                
+
+                
+                HStack{
+                    Spacer()
+                    Image(systemName: "fork.knife.circle")
+                    Image(systemName: "binoculars.fill")
+                }
+                .foregroundColor(Color.gray)
+                .font(.caption)
+                
+            }
+            .padding(5)
+            .background(Rectangle()
+                .foregroundColor(.white))
+                .padding(10)
+                .shadow(radius: 20)
         }
-        .padding()
     }
 }
 
